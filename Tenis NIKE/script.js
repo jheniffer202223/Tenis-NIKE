7024f8efca0906884670f55ee9dd7c870483e761
jheniffee__00 <jhenifferalvessantos8@gmail.com>
2025-06-23 21:28:33 -0300

    Add files via upload

---
+++ Tenis NIKE/script.js
@@ -0,0 +1,40 @@
+var produtos = [
+ {
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfoZtCnNC6hfeO4bSBfG7MryO0v4X-KXDKcw&s",
+    titulo: "Nike air Max 90",
+    descricao: "Eleve seu estilo com o clássico reinventado. Conforto supremo encontra design inovador",
+    preco: "R$ 899",
+
+ },
+ {
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfoZtCnNC6hfeO4bSBfG7MryO0v4X-KXDKcw&s",
+    titulo: "Nike air Max 100",
+    descricao: "Eleve seu estilo com o clássico reinventado. Conforto supremo encontra design inovador",
+    preco: "R$ 899",
+
+ },
+ {
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfoZtCnNC6hfeO4bSBfG7MryO0v4X-KXDKcw&s",
+    titulo: "Nike air Max 110",
+    descricao: "Eleve seu estilo com o clássico reinventado. Conforto supremo encontra design inovador",
+    preco: "R$ 899",
+
+ },
+ {
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfoZtCnNC6hfeO4bSBfG7MryO0v4X-KXDKcw&s",
+    titulo: "Nike air Max 120",
+    descricao: "Eleve seu estilo com o clássico reinventado. Conforto supremo encontra design inovador",
+    preco: "R$ 899",
+
+ },
+ {
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfoZtCnNC6hfeO4bSBfG7MryO0v4X-KXDKcw&s",
+    titulo: "Nike air Max ",
+    descricao: "Eleve seu estilo com o clássico reinventado. Conforto supremo encontra design inovador",
+    preco: "R$ 899",
+
+ },
+ 
+]
+
+var divProdutos = document.querySe
